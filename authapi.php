a6c1eb5b53fe3dad8d784b9cdca03a4e5786f75a
d3se <135171210+d3se@users.noreply.github.com>
2023-07-23 15:00:52 +0200

    Add files via upload

--- authapi.php
+++ authapi.php
@@ -4,25 +4,46 @@ header('Content-Type: application/json');
 $db = new PDO('mysql:host=localhost;dbname=dbname;charset=utf8', 'user', 'pass'); 
 
 $data = json_decode(file_get_contents('php://input'), true);
-$username = $data['username'];
-$password = $data['password'];
+$username = filter_var($data['username'], FILTER_SANITIZE_STRING);
+$password = filter_var($data['password'], FILTER_SANITIZE_STRING);
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$file = 'ratelimits.txt';
+$lines = file($file);
+$limit = 10;  
+$timeout = 60;
+
+foreach ($lines as $line) {
+    list($old_ip, $old_time) = explode(',', $line);
+    if ($old_ip == $ip && time() - $old_time < $timeout) {
+        die(json_encode(['success' => false, 'message' => 'Rate limit exceeded']));
+    }
+}
+
+file_put_contents($file, "$ip," . time() . "\n", FILE_APPEND);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') { 
-    if (strpos($_SERVER['REQUEST_URI'], '/register') !== false) { 
-        $stmt = $db->prepare('INSERT INTO users (username, password) VALUES (?, ?)'); 
-        $result = $stmt->execute([$username, password_hash($password, PASSWORD_DEFAULT)]); 
-        echo json_encode(['success' => $result]); 
-    } 
-    elseif (strpos($_SERVER['REQUEST_URI'], '/login') !== false) { 
-        $stmt = $db->prepare('SELECT password FROM users WHERE username = ?'); 
-        $stmt->execute([$username]); 
-        $result = $stmt->fetch(PDO::FETCH_ASSOC); 
-
-        if ($result && password_verify($password, $result['password'])) { 
-            echo json_encode(['success' => true]); 
-        } 
-        else { 
-            echo json_encode(['success' => false]); 
-        } 
-    } 
+    try {
+        if ($_GET['url'] === "register") { 
+            $stmt = $db->prepare('INSERT INTO users (username, password) VALUES (?, ?)');
+            $stmt->bindParam(1, $username);
+            $stmt->bindParam(2, password_hash($password, PASSWORD_DEFAULT));
+            $result = $stmt->execute();
+            echo json_encode(['success' => $result]);
+        } else if ($_GET['url'] === "login") { 
+            $stmt = $db->prepare('SELECT password FROM users WHERE username = ?');
+            $stmt->bindParam(1, $username);
+            $stmt->execute();
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($row && password_verify($password, $row['password'])) {
+                echo json_encode(['success' => true]);
+            } else {
+                echo json_encode(['success' => false]);
+            }
+        }
+    } catch (PDOException $ex) {
+        error_log($ex->getMessage());
+        die(json_encode(['success' => false]));
+    }
 }
+?>
