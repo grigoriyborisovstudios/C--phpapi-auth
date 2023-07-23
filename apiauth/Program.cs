e0c324ca9e02f782f0c715881fa3c818db142894
d3se <135171210+d3se@users.noreply.github.com>
2023-07-23 15:03:19 +0200

    1.1.0

--- apiauth/Program.cs
+++ apiauth/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -18,7 +20,7 @@ public class Program
         string username = Console.ReadLine();
 
         Console.WriteLine("Please enter your password:");
-        string password = Console.ReadLine();
+        SecureString password = GetPasswordFromConsole();
 
         if (choice == "1")
         {
@@ -26,7 +28,7 @@ public class Program
         }
         else if (choice == "2")
         {
-            await Login(username, password);
+            await Login(username, password); //2
         }
         else
         {
@@ -36,43 +38,94 @@ public class Program
         Console.ReadKey();
     }
 
-    static async Task Register(string username, string password)
+    public static SecureString GetPasswordFromConsole()
     {
-        var requestData = new { username = username, password = password };
+        var password = new SecureString();
+        while (true)
+        {
+            ConsoleKeyInfo i = Console.ReadKey(true);
+            if (i.Key == ConsoleKey.Enter)
+            {
+                break;
+            }
+            else if (i.Key == ConsoleKey.Backspace)
+            {
+                if (password.Length > 0)
+                {
+                    password.RemoveAt(password.Length - 1);
+                    Console.Write("\b \b");
+                }
+            }
+            else
+            {
+                password.AppendChar(i.KeyChar);
+                Console.Write("*");
+            }
+        }
+        return password;
+    }
+
+    static async Task Register(string username, SecureString password)
+    {
+        // Unsecured password conversion. This is insecure and used just for illustration.
+        var passwordBstr = Marshal.SecureStringToBSTR(password);
+        var insecurePassword = Marshal.PtrToStringBSTR(passwordBstr);
+        Marshal.ZeroFreeBSTR(passwordBstr);
+
+        var requestData = new { username = username, password = insecurePassword };
         var content = new StringContent(JsonConvert.SerializeObject(requestData), Encoding.UTF8, "application/json");
 
-        HttpResponseMessage response = await client.PostAsync(baseUrl + "/register", content);
-
-        string responseStr = await response.Content.ReadAsStringAsync();
-        dynamic responseObj = JsonConvert.DeserializeObject(responseStr);
-
-        if (responseObj != null && responseObj.success == true)
+        try
         {
-            Console.WriteLine("Registration successful.");
+            HttpResponseMessage response = await client.PostAsync(baseUrl + "/register", content);
+
+            string responseStr = await response.Content.ReadAsStringAsync();
+            dynamic responseObj = JsonConvert.DeserializeObject(responseStr);
+
+            if (responseObj != null && responseObj.success == true)
+            {
+                Console.WriteLine("Registration successful.");
+            }
+            else
+            {
+                Console.WriteLine("Registration failed. Response: " + responseStr);
+            }
         }
-        else
+        catch (Exception ex)
         {
-            Console.WriteLine("Registration failed. Response: " + responseStr);
+            Console.WriteLine("An error occurred: " + ex.Message);
         }
     }
 
-    static async Task Login(string username, string password)
+    static async Task Login(string username, SecureString password)
     {
-        var requestData = new { username = username, password = password };
+        // Unsecured password conversion. This is insecure and used just for illustration.
+        var passwordBstr = Marshal.SecureStringToBSTR(password);
+        var insecurePassword = Marshal.PtrToStringBSTR(passwordBstr);
+        Marshal.ZeroFreeBSTR(passwordBstr);
+
+        var requestData = new { username = username, password = insecurePassword };
         var content = new StringContent(JsonConvert.SerializeObject(requestData), Encoding.UTF8, "application/json");
 
-        HttpResponseMessage response = await client.PostAsync(baseUrl + "/login", content);
-
-        string responseStr = await response.Content.ReadAsStringAsync();
-        dynamic responseObj = JsonConvert.DeserializeObject(responseStr);
-
-        if (responseObj != null && responseObj.success == true)
+        try
         {
-            Console.WriteLine("Login successful.");
+            HttpResponseMessage response = await client.PostAsync(baseUrl + "/login", content);
+
+            string responseStr = await response.Content.ReadAsStringAsync();
+            dynamic responseObj = JsonConvert.DeserializeObject(responseStr);
+
+            if (responseObj != null && responseObj.success == true)
+            {
+                Console.WriteLine("Login successful.");
+            }
+            else
+            {
+                Console.WriteLine("Login failed. Response: " + responseStr);
+            }
         }
-        else
+        catch (Exception ex)
         {
-            Console.WriteLine("Login failed. Response: " + responseStr);
+            Console.WriteLine("An error occurred: " + ex.Message);
         }
     }
 }
